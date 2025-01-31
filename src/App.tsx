a2fa4e7b9bf7fe44eeb959f3225ce54dab101c9b
Jaewon <wellbird@naver.com>
2025-01-31 19:47:56 +0900

    feat : 테마 토글 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { ThemeProvider } from 'styled-components';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
-import { GlobalStyle } from './style/global';
-import { dark, light } from './style/theme';
+import ThemeSwitcher from './components/header/ThemeSwitcher';
+import { BookStoreThemeProvider } from './context/themeContext';
 
 function App() {
   return (
-    <ThemeProvider theme={light}>
-      <GlobalStyle themeName="light" />
+    <BookStoreThemeProvider>
+      <ThemeSwitcher />
       <Layout>
         <Home />
       </Layout>
-    </ThemeProvider>
+    </BookStoreThemeProvider>
   );
 }
 
