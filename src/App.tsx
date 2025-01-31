ccd07c141ab9d1d8bad88fd2cd01f1b973f0d3f8
Jaewon <wellbird@naver.com>
2025-01-31 18:50:08 +0900

    style : 테마 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from 'styled-components';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
+import { GlobalStyle } from './style/global';
+import { dark, light } from './style/theme';
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <ThemeProvider theme={light}>
+      <GlobalStyle themeName="light" />
+      <Layout>
+        <Home />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
