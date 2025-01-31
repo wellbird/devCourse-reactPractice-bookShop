750f595dc4fab7e8372eed8ee632905f1a4d86be
Jaewon <wellbird@naver.com>
2025-01-31 18:36:07 +0900

    style : globalStyle 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <div>
+    <HeaderStyle>
       <h1>Book Store</h1>
-    </div>
+    </HeaderStyle>
   );
 };
+
+const HeaderStyle = styled.header`
+  background-color: #333;
+  h1 {
+    color: white;
+  }
+`;
 
 export default Header;
