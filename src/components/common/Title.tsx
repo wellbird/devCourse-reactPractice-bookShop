69962a206686504523d891599c0ed50dcb441ecb
Jaewon <wellbird@naver.com>
2025-02-03 21:35:36 +0900

    feat : title 컴포넌트 구현

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ColorKey, HeadingSize } from '../../style/theme';
+
+interface Props {
+  children: React.ReactNode;
+  size: HeadingSize;
+  color?: ColorKey;
+}
+
+const Title: React.FC<Props> = ({ children, size, color }) => {
+  return (
+    <TitleStyle size={size} color={color}>
+      {children}
+    </TitleStyle>
+  );
+};
+
+const TitleStyle = styled.h1<Omit<Props, 'children'>>`
+  font-size: ${({ theme, size }) => theme.heading[size].fontSize};
+  color: ${({ theme, color }) =>
+    color ? theme.color[color] : theme.color.primary};
+`;
+
+export default Title;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Title from '../components/common/Title';
 
 const Home = () => {
   return (
     <>
+      <Title size="large">제목테스트</Title>
       <div>home body</div>
     </>
   );
