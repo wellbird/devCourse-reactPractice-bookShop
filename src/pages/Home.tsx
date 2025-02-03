8d68e93166a575b65ac3cc1550809cebd2949d13
Jaewon <wellbird@naver.com>
2025-02-03 21:56:23 +0900

    feat : button 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Title from '../components/common/Title';
+import Button from '../components/common/Button';
 
 const Home = () => {
   return (
     <>
-      <Title size="large">제목테스트</Title>
+      <Title size="large">제목 테스트</Title>
+      <Button size="large" scheme="normal">
+        버튼 테스트
+      </Button>
       <div>home body</div>
     </>
   );
