01ab26442927b8de90a62f2dcae51ae413dc6353
Jaewon <wellbird@naver.com>
2025-02-03 23:13:47 +0900

    feat : footer, header 구체화

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,6 @@ import InputText from '../components/common/Input';
 const Home = () => {
   return (
     <>
-      <Title size="large">제목 테스트</Title>
-      <Button size="large" scheme="normal">
-        버튼 테스트
-      </Button>
-      <InputText placeholder="여기에 입력하세요" />
       <div>home body</div>
     </>
   );
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -9,6 +9,7 @@ export type ColorKey =
 export type HeadingSize = 'large' | 'medium' | 'small';
 export type ButtonSize = 'large' | 'medium' | 'small';
 export type ButtonScheme = 'primary' | 'normal';
+export type LayoutWidth = 'large' | 'medium' | 'small';
 
 interface Theme {
   name: ThemeName;
@@ -33,14 +34,19 @@ interface Theme {
   borderRadius: {
     default: string;
   };
+  layout: {
+    width: {
+      [key in LayoutWidth]: string;
+    };
+  };
 }
 
 export const light: Theme = {
   name: 'light',
   color: {
-    primary: 'brown',
-    background: 'lightgray',
-    secondary: 'blue',
+    primary: '#ff5800',
+    background: 'lightgrey',
+    secondary: '#5f5f5f',
     third: 'green',
     border: 'grey',
     text: 'black',
@@ -82,6 +88,13 @@ export const light: Theme = {
   },
   borderRadius: {
     default: '4px',
+  },
+  layout: {
+    width: {
+      large: '1020px',
+      medium: '760px',
+      small: '320px',
+    },
   },
 };
 
