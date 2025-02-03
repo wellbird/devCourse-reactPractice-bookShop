2e3b7d71768d6fc43c43eb4ed451510011785e49
Jaewon <wellbird@naver.com>
2025-02-03 21:35:47 +0900

    test : title 컴포넌트 테스트 코드 작성

---
+++ src/components/common/Title.spec.tsx
@@ -0,0 +1,41 @@
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Title from './Title';
+import { BookStoreThemeProvider } from '../../context/themeContext';
+
+describe('Title 컴포넌트 테스트', () => {
+  it('랜더를 확인', () => {
+    // 랜더
+    render(
+      <BookStoreThemeProvider>
+        <Title size="large">제목</Title>
+      </BookStoreThemeProvider>
+    );
+    // 확인
+    expect(screen.getByText('제목')).toBeInTheDocument();
+  });
+  it('size props 적용', () => {
+    // 랜더
+    const { container } = render(
+      <BookStoreThemeProvider>
+        <Title size="large">제목</Title>
+      </BookStoreThemeProvider>
+    );
+    // 확인
+    expect(container?.firstChild).toHaveStyle({ fontSize: '2rem' });
+  });
+
+  it('color props 적용', () => {
+    // 랜더
+    const { container } = render(
+      <BookStoreThemeProvider>
+        <Title size="large" color="primary">
+          제목
+        </Title>
+      </BookStoreThemeProvider>
+    );
+    // 확인
+    expect(container?.firstChild).toHaveStyle({ color: 'rgb(165, 42, 42)' });
+  });
+});
